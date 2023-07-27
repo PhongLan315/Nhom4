c0a97cd698cec5fbf74fd90c700589c88b2d9018
tanpingg <139566925+tanpingg@users.noreply.github.com>
2023-07-27 10:28:25 +0700

    Add files via upload

---
+++ CuoiKy/LoginResgistration/src/mainForm/mainForm.java
@@ -0,0 +1,17 @@
+package mainForm;
+
+import javax.swing.JFrame;
+
+public class mainForm extends JFrame{
+
+    public mainForm() {
+        setSize(450, 250);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setVisible(true);
+        
+    }
+
+}
+
